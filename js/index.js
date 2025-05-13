5e3116560f5f75442f0abbb199b6941a5a1811a1
JakeShaju <95337441+JakeShaju@users.noreply.github.com>
2025-05-13 10:11:34 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
+let a = 3;
+let b = 5;
+let c;
+
+let output = `
+a = ${a}
+b = ${b}
+c = ${c}
+-------------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+
+a += b => ${a += b}
+a -= b => ${a -= b}
+a *= b => ${a *= b}
+a /= b => ${a /= b}
+a %= b => ${a %= b}
+
+a == b => ${a == b}
+a != b => ${a != b}
+a > b => ${a > b}
+a < b => ${a < b}
+
+!a && !c => ${!a && !c}
+!a || !c => ${!a || !c}
+`
+
+console.log(output);
 
